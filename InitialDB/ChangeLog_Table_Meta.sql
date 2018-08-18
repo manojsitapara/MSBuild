518f73586cc2fca0445be56acdb71381986f634c
Manoj Sitapara <manojsitapara@users.noreply.github.com>
2018-08-18 16:25:51 +0530

    1. Added following reserved itemgroup and property inside inputs parameter of BeforeCompiletarget - because whenever any dependency project build, project assembly needs to update or if there is only solution file change (like added only config file) in that case as well project assembly needs to build and update version @(ReferencePath); $(MSBuildAllProjects);
    2. Added functinality to take backup of dbDeply database
    
    3. Moved out propertygroup of pertaining to hold decryptvalue into common property group
    
    4. Removed unused propertygroup from target DownloadPreBuildSourceFromSVN
    
    5. Renamed taget from InitilizeItemGroup to InitializeItemGroup
    
    6. Removed following targets, because now we are able to handle these functionalities by generic target BuildTPASolutions
    BuildGDISolution
    BuildPrimePaySolution
    BuildPayFlexSolution
    
    7. Set LayoutFiles property in following targets because we are now using CXCFileReader, CXCFileReader expect to have a layout fie
    -CommonCommandFileTransform
    -TPASpecificCommandFileTransform
    
    8. Added target DeleteOldLogFile to delete old msbuild readme file on destination server

---
+++ InitialDB/ChangeLog_Table_Meta.sql
@@ -0,0 +1,51 @@
+USE [MsBuildDbDeploy]
+GO
+SET IDENTITY_INSERT [dbo].[changelog] ON 
+
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (1, N'1.CXCCore_SchemaChanges', 1, N'1.Ticket_24607.sql', CAST(N'2018-08-14 22:29:47.043' AS DateTime), CAST(N'2018-08-14 22:29:47.120' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (2, N'1.CXCCore_SchemaChanges', 2, N'2.Ticket_25570.sql', CAST(N'2018-08-14 22:29:47.120' AS DateTime), CAST(N'2018-08-14 22:29:47.123' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (3, N'1.CXCCore_SchemaChanges', 3, N'3.Ticket_25615.sql', CAST(N'2018-08-14 22:29:47.123' AS DateTime), CAST(N'2018-08-14 22:29:47.127' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (4, N'1.CXCCore_SchemaChanges', 4, N'4.Ticket_25647.sql', CAST(N'2018-08-14 22:29:47.127' AS DateTime), CAST(N'2018-08-14 22:29:47.130' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (5, N'1.CXCCore_SchemaChanges', 5, N'5.Ticket_25693.sql', CAST(N'2018-08-14 22:29:47.130' AS DateTime), CAST(N'2018-08-14 22:29:47.133' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (6, N'1.CXCCore_SchemaChanges', 6, N'6.Ticket_26484.sql', CAST(N'2018-08-14 22:29:47.137' AS DateTime), CAST(N'2018-08-14 22:29:47.140' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (7, N'1.CXCCore_SchemaChanges', 7, N'7.Ticket_26330.sql', CAST(N'2018-08-14 22:29:47.140' AS DateTime), CAST(N'2018-08-14 22:29:47.143' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (8, N'1.CXCCore_SchemaChanges', 8, N'8.Ticket_27213.sql', CAST(N'2018-08-14 22:29:47.147' AS DateTime), CAST(N'2018-08-14 22:29:47.177' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (9, N'1.CXCCore_SchemaChanges', 9, N'9.Ticket_27277.sql', CAST(N'2018-08-14 22:29:47.180' AS DateTime), CAST(N'2018-08-14 22:29:47.187' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (10, N'1.CXCCore_SchemaChanges', 10, N'10.Ticket_27384.sql', CAST(N'2018-08-14 22:29:47.190' AS DateTime), CAST(N'2018-08-14 22:29:47.197' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (11, N'1.CXCCore_SchemaChanges', 11, N'11.Ticket_27540.sql', CAST(N'2018-08-14 22:29:47.200' AS DateTime), CAST(N'2018-08-14 22:29:47.207' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (12, N'1.CXCCore_SchemaChanges', 12, N'12.Ticket_27460.sql', CAST(N'2018-08-14 22:29:47.207' AS DateTime), CAST(N'2018-08-14 22:29:47.210' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (13, N'1.CXCCore_SchemaChanges', 13, N'13.Ticket_28136.sql', CAST(N'2018-08-14 22:29:47.217' AS DateTime), CAST(N'2018-08-14 22:29:47.220' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (14, N'1.CXCCore_SchemaChanges', 14, N'14.Ticket_31751.sql', CAST(N'2018-08-14 22:29:47.220' AS DateTime), CAST(N'2018-08-14 22:29:47.223' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (15, N'4.GDI_MetaChanges', 1, N'1.Ticket_27383.sql', CAST(N'2018-08-14 22:29:47.227' AS DateTime), CAST(N'2018-08-14 22:29:47.277' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (16, N'4.GDI_MetaChanges', 2, N'2.Ticket_28949.sql', CAST(N'2018-08-14 22:29:47.277' AS DateTime), CAST(N'2018-08-14 22:29:47.300' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (17, N'4.GDI_MetaChanges', 3, N'3.Ticket_29056.sql', CAST(N'2018-08-14 22:29:47.303' AS DateTime), CAST(N'2018-08-14 22:29:47.307' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (18, N'4.GDI_MetaChanges', 4, N'4.Ticket_29069.sql', CAST(N'2018-08-14 22:29:47.310' AS DateTime), CAST(N'2018-08-14 22:29:47.317' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (19, N'4.GDI_MetaChanges', 5, N'5.Ticket_29715.sql', CAST(N'2018-08-14 22:29:47.317' AS DateTime), CAST(N'2018-08-14 22:29:47.320' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (20, N'4.GDI_MetaChanges', 6, N'6.Ticket_32082.sql', CAST(N'2018-08-14 22:29:47.327' AS DateTime), CAST(N'2018-08-14 22:29:47.330' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (21, N'4.PrimePay_MetaChanges', 1, N'1.Ticket_30561.sql', CAST(N'2018-08-14 22:29:47.330' AS DateTime), CAST(N'2018-08-14 22:29:47.413' AS DateTime), N'dbo', 1, N'')
+GO
+INSERT [dbo].[changelog] ([ChangeId], [Folder], [ScriptNumber], [ScriptName], [StartDate], [CompleteDate], [AppliedBy], [ScriptStatus], [ScriptOutput]) VALUES (22, N'Dummy', 1, N'DummyScript', CAST(N'2018-08-14 22:29:47.543' AS DateTime), CAST(N'2018-08-14 22:29:47.543' AS DateTime), N'dbo', 1, N'2018.08.02.0')
+GO
+SET IDENTITY_INSERT [dbo].[changelog] OFF
+GO
